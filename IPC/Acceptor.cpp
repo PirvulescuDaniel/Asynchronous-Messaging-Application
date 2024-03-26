d7beb50ced6735a02ebe12e8659027196348d71d
PirvulescuDaniel <pirvulescu.daniel24@yahoo.com>
2024-03-26 20:17:23 +0200

    start implementing the acceptor class.

---
+++ IPC/Acceptor.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "Acceptor.h"
+
+void Acceptor::StartAccepting()
+{
+	//TODO
+}
+
+void Acceptor::Stop()
+{
+	//TODO
+}
+
+void Acceptor::InitAccept()
+{
+	//TODO
+}
+
+void Acceptor::OnAccept(const asio::error_code& aErrorCode, CommunicationSocket aCommunicationSocket)
+{
+	//TODO
+}
---
+++ IPC/Acceptor.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "pch.h"
+
+class Acceptor
+{
+public:
+
+	using CommunicationSocket = std::shared_ptr<asio::ip::tcp::socket>;
+
+	Acceptor(asio::io_service& aIoService, unsigned short aPort)
+		:mIoService(aIoService)
+		, mAcceptor(mIoService, asio::ip::tcp::endpoint(asio::ip::address_v4::any(), aPort))
+	{
+	}
+
+	/*
+		Non-blocking method that starts the process of accepting connection requests from clients.
+	*/
+	void StartAccepting();
+
+	/*
+		Stops the accepting process.
+	*/
+	void Stop();
+
+private:
+
+	/*
+		Waits for connection requests from clients.
+	*/
+	void InitAccept();
+
+	/*
+		It is called after a connection request has been accepted.
+	*/
+	void OnAccept(const asio::error_code& aErrorCode, CommunicationSocket aCommunicationSocket);
+
+	asio::io_service&       mIoService;
+	asio::ip::tcp::acceptor mAcceptor;
+};

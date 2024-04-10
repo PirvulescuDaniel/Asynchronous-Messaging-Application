e61149b82b0eb63365e134847825c1002b592cf3
PirvulescuDaniel <pirvulescu.daniel24@yahoo.com>
2024-04-11 00:25:01 +0300

    Continue work at DiscoveryManager

--- IPC/DiscoveryManager.cpp
+++ IPC/DiscoveryManager.cpp
@@ -4,21 +4,65 @@
 
 DiscoveryManager::DiscoveryManager(std::string_view aBroadcastIP, unsigned short aPort)
 	:mBroadcaster(new UDPBroadcastSender(mIoService, aBroadcastIP, aPort))
+	,mTimer(nullptr)
 {
 	mWorker.reset(new asio::io_service::work(mIoService));
+
+	mThreadWorker.reset(new std::thread(
+		[this]()
+		{
+			mIoService.run();
+		}
+	));
 }
 
-void DiscoveryManager::Broadcast()
+DiscoveryManager::~DiscoveryManager()
 {
-	mBroadcaster->BroadcastAsync(mMessageHello);
+	mBroadcaster->BroadcastSync(mMessageBye);
+
+	mWorker.reset(nullptr);
+
+	if (mThreadWorker && mThreadWorker->joinable())
+		mThreadWorker->join();
 }
 
-void DiscoveryManager::StartRecurrentBroadcasting()
+void DiscoveryManager::Broadcast(const std::string& aMessage)
 {
-	// TODO
+	mBroadcaster->BroadcastAsync(aMessage);
+}
+
+void DiscoveryManager::StartRecurrentBroadcasting(BroadcastDelay aDelay)
+{
+	// Do not start another recurrent broadcasting if another is already in progress.
+	if (!mBroadcastStoped)
+		return;
+
+	mBroadcastStoped = false;
+	mTimer.reset(new asio::steady_timer(mIoService, aDelay));
+	
+	if(mTimer)
+		mTimer->async_wait(std::bind(&DiscoveryManager::HandleTimeExpired, this, std::placeholders::_1, aDelay));
 }
 
 void DiscoveryManager::StopRecurrentBroadcasting()
 {
-	// TODO
+	mBroadcastStoped = true;
+}
+
+void DiscoveryManager::HandleTimeExpired(const asio::error_code& aErrorCode, BroadcastDelay aDelay)
+{
+	// Stop the recurrent broadcasting process if errors have occurred.
+	if (aErrorCode.value() != 0)
+	{
+		mBroadcastStoped = true;
+		return;
+	}
+
+	if (mBroadcastStoped)
+		return;
+
+	mBroadcaster->BroadcastAsync(mMessageHello);
+
+	mTimer->expires_at(mTimer->expiry() + aDelay);
+	mTimer->async_wait(std::bind(&DiscoveryManager::HandleTimeExpired, this, std::placeholders::_1, aDelay));
 }
--- IPC/DiscoveryManager.h
+++ IPC/DiscoveryManager.h
@@ -8,26 +8,48 @@ class DiscoveryManager
 public:
 
 	using WorkerPtr      = std::unique_ptr<asio::io_service::work>;
+	using ThreadWorker   = std::unique_ptr<std::thread>;
 	using BroadcasterPtr = std::unique_ptr<UDPBroadcastSender>;
 	using Endpoint       = asio::ip::udp::endpoint;
+	using BroadcastDelay = std::chrono::seconds;
+	using TimerPtr       = std::unique_ptr<asio::steady_timer>;
 
 	DiscoveryManager(std::string_view aBroadcastIP, unsigned short aPort);
+
+	~DiscoveryManager();
 
 	/*
 		It broadcasts only once, asynchronously.
 	*/
-	void Broadcast();
+	void Broadcast(const std::string& aMessage);
 
-	void StartRecurrentBroadcasting();
+	/*
+		Start the broadcasting process at an interval of aDelay seconds.
+	*/
+	void StartRecurrentBroadcasting(BroadcastDelay aDelay);
 
+	/*
+		Stops the current recurrent broadcasting process.
+	*/
 	void StopRecurrentBroadcasting();
 
 private:
+
+	/*
+		Handle the expiration of the timer.
+		If there are errors, we stop the process. otherwise we do another broadcast.
+	*/
+	void HandleTimeExpired(const asio::error_code& aErrorCode, BroadcastDelay aDelay);
+
 	asio::io_service   mIoService;
 	WorkerPtr          mWorker;
+	ThreadWorker       mThreadWorker;
 
-	BroadcasterPtr mBroadcaster;
+	BroadcasterPtr   mBroadcaster;
+	std::atomic_bool mBroadcastStoped {true};
 
 	std::string mMessageHello {"Hello"};
 	std::string mMessageBye		{"Bye"};
+
+	TimerPtr mTimer;
 };
